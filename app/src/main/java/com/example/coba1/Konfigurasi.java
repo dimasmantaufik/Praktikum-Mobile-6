69835aab2fee1d8285d215a9bb2bb7656729463c
dimasmantaufik <64480447+dimasmantaufik@users.noreply.github.com>
2020-05-14 23:38:44 +0700

    Praktikum modul 6 Pemrograman Mobile

---
+++ app/src/main/java/com/example/coba1/Konfigurasi.java
@@ -0,0 +1,22 @@
+package com.example.coba1;
+
+public class Konfigurasi {
+    public static final String URL_ADD= "http://192.168.1.67/db_android/insert.php";
+    public static final String URL_GET_ALL = "http://192.168.1.67/db_android/read.php";
+    public static final String URL_GET_MHS = "http://192.168.1.67/db_android/select.php?id=";
+    public static final String URL_UPDATE_MHS = "http://192.168.1.67/db_android/update.php";
+    public static final String URL_DELETE_MHS = "http://192.168.1.67/db_android/delete.php?id=";
+
+    public static final String KEY_MHS_ID = "id";
+    public static final String KEY_MHS_NAMA = "nama";
+    public static final String KEY_MHS_JURUSAN = "jurusan";
+    public static final String KEY_MHS_EMAIL = "email";
+
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_JURUSAN = "jurusan";
+    public static final String TAG_EMAIL = "email";
+
+    public static final String MHS_ID = "mhs_id";
+}
